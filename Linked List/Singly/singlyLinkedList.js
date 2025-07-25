5dd0c4944420d98083b9f709eb34097c1cb01171
Mahesh Das <work.maheshprasad@gmail.com>
2025-07-25 21:47:05 +0530

    Implement methods for adding, deleting, searching, and reversing nodes in SinglyLinkedList

--- Linked List/Singly/singlyLinkedList.js
+++ Linked List/Singly/singlyLinkedList.js
@@ -25,6 +25,64 @@ class SinglyLinkedList{
             current.next = newNode
         }
     }
+
+    addAtStart(data){
+        const newNode = new Node(data);
+        if(!this.head){
+            this.head = newNode;
+        }else
+        {
+            newNode.next = this.head
+            this.head = newNode
+        }
+    }
+
+    deleteByValue(value){
+        if(!this.head) return
+
+        if(this.head.data == value){
+            this.head = this.head.next
+            return
+        }
+        else{
+            let current = this.head;
+            
+            while(current.next && current.next.data !== value){
+                current = current.next
+            }
+
+            if(current.next){
+                current.next = current.next.next
+            }
+        }
+    }
+
+    searchByValue(value){
+        if(!this.head) return null
+
+        let current = this.head;
+        while(current){
+            if(current.data == value) console.log("Value present :",current);
+            ;
+            current = current.next;
+        }
+        console.log("Value not Found");
+        
+    }
+
+    reverse(){
+        let current = this.head;
+        let prev = null
+
+        while(current){
+            let nextNode = current.next;
+            current.next = prev;
+            prev = current
+            current = nextNode
+        }
+        this.head = prev
+    }
+
     print(){
         let current = this.head;
         const values = []
@@ -43,5 +101,12 @@ list.append(10);
 list.append(20);
 list.append(30);
 list.append(40);
-
+list.print()
+list.addAtStart(5)
+list.print()
+list.deleteByValue(10)
+list.print()
+list.searchByValue(5)
+list.print()
+list.reverse()
 list.print()
